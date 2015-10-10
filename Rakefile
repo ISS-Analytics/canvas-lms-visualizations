5db955b2b4215b323d773f001c14b19736c08048
James Uanhoro <stonegold546@gmail.com>
2015-10-10 18:14:16 +0800

    Further security
    - Added rake, rack-ssl-enforcer, rbnacl-libsodium to Gemfile
    - Updated config_env example
    - Added rake_tasks to Rakefile to share config_env with heroku

---
+++ Rakefile
@@ -0,0 +1,2 @@
+require './app.rb'
+require 'config_env/rake_tasks'
--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 require 'sinatra'
+require 'config_env'
+require 'rack/ssl-enforcer'
+
+configure :development, :test do
+  ConfigEnv.path_to_config("#{__dir__}/config/config_env.rb")
+end
 
 # Custom ~'wrapper' for the Canvas LMS API
 class CanvasLmsAPI < Sinatra::Base
+  configure :production do
+    use Rack::SslEnforcer
+    set :session_secret, ENV['MSG_KEY']
+  end
+
   get '/' do
     'Hello'
   end
