a5ee3ba289d0cfff45395d470d5a8c4015adb83d
James Uanhoro <stonegold546@gmail.com>
2015-10-10 22:02:17 +0800

    Testing Canvas API use
    - Added httparty gem
    - Added route to retrieve list of classes/test

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'config_env'
 require 'rack/ssl-enforcer'
+require 'httparty'
 
 configure :development, :test do
   ConfigEnv.path_to_config("#{__dir__}/config/config_env.rb")
@@ -13,7 +14,16 @@ class CanvasLmsAPI < Sinatra::Base
     set :session_secret, ENV['MSG_KEY']
   end
 
+  CANVAS = 'https://n.acme.instructure.com/api/v1/'
+
   get '/' do
     'Hello'
+  end
+
+  # Get course list
+  get '/course_list/?' do
+    url = CANVAS + 'courses'
+    headers = { authorization: ('Bearer ' + ENV['ACCESS_TOKEN']) }
+    (HTTParty.get url, headers: headers).to_s
   end
 end
