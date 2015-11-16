2a4feb4f3c112302f363a348e7f45ab9f494d48c
James Uanhoro <stonegold546@gmail.com>
2015-11-16 15:47:49 +0800

    Added Postgres to production

--- environments.rb
+++ environments.rb
@@ -7,3 +7,16 @@ configure :test do
   set :database, 'sqlite3:db/test.db'
   set :show_exceptions, true
 end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    adapter:  db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    host:     db.host,
+    username: db.user,
+    password: db.password,
+    database: db.path[1..-1],
+    encoding: 'utf8'
+  )
+end
