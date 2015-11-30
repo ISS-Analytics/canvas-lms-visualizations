db48976c4a15b35e79488caaa2e65566e3665455
James Uanhoro <stonegold546@gmail.com>
2015-11-30 17:34:43 +0800

    Refactored course_info & course_analytics API calls

--- helpers/app_api_helpers.rb
+++ helpers/app_api_helpers.rb
@@ -8,52 +8,44 @@ module AppAPIHelper
     elsif params['data'] == 'users'
       user_by_user(*arr)
     elsif params['no_analytics']
-      course_info(*arr)
-    else course_analytics(*arr)
+      GetCourseInfoFromCanvas.new(*arr).call
+    else GetCourseAnalyticsFromCanvas.new(*arr).call
     end
   end
 
-  def course_analytics(canvas_api, canvas_token, course_id, data)
-    url = canvas_api + 'courses/' + course_id + "/analytics/#{data}"
-    visit_canvas = VisitCanvasAPI.new(url, canvas_token)
-    visit_canvas.call
-  end
-
-  def course_info(canvas_api, canvas_token, course_id, data)
-    url = canvas_api + 'courses/' + course_id + "/#{data}"
-    visit_canvas = VisitCanvasAPI.new(url, canvas_token)
-    visit_canvas.call
-  end
-
   def all_discussion(canvas_api, canvas_token, course_id, data)
-    discussions = course_info(canvas_api, canvas_token, course_id, data)
+    discussions = GetCourseInfoFromCanvas.new(canvas_api, canvas_token,
+                                              course_id, data).call
     discussions = JSON.parse(discussions)
     discussions.map do |discussion|
       Concurrent::Future.new do
-        course_info(canvas_api, canvas_token, course_id,
-                    "/#{data}/#{discussion['id']}/view")
+        GetCourseInfoFromCanvas.new(canvas_api, canvas_token, course_id,
+                                    "/#{data}/#{discussion['id']}/view").call
       end; end.map(&:execute).map(&:value).to_json
   end
 
   def all_quiz(canvas_api, canvas_token, course_id, data)
-    quizzes = course_info(canvas_api, canvas_token, course_id, data)
+    quizzes = GetCourseInfoFromCanvas.new(canvas_api, canvas_token, course_id,
+                                          data).call
     quizzes = JSON.parse(quizzes)
     quizzes.map do |quiz|
       Concurrent::Future.new do
-        { quiz['title'] => course_info(canvas_api, canvas_token, course_id,
-                                       "/#{data}/#{quiz['id']}/statistics") }
+        { quiz['title'] => GetCourseInfoFromCanvas.new(
+          canvas_api, canvas_token, course_id,
+          "/#{data}/#{quiz['id']}/statistics").call }
       end; end.map(&:execute).map(&:value).to_json
   end
 
   def user_by_user(canvas_api, canvas_token, course_id, data)
-    persons = course_info(canvas_api, canvas_token, course_id, 'enrollments')
+    persons = GetCourseInfoFromCanvas.new(canvas_api, canvas_token, course_id,
+                                          'enrollments').call
     student_ids = JSON.parse(persons, quirks_mode: true).map do |person|
       person['user_id'] if person['type'] == 'StudentEnrollment'
     end
     student_ids.compact.map do |id|
       Concurrent::Future.new do
-        { "#{id}" => course_analytics(canvas_api, canvas_token, course_id,
-                                      "/#{data}/#{id}/activity") }
+        { "#{id}" => GetCourseAnalyticsFromCanvas.new(
+          canvas_api, canvas_token, course_id, "/#{data}/#{id}/activity").call }
       end; end.map(&:execute).map(&:value).to_json
   end
 end
