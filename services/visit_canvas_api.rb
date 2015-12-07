b96b4f26ed92cae72b638fe68d7660394b13b470
James Uanhoro <stonegold546@gmail.com>
2015-12-07 15:33:49 +0800

    Fixed partial data return issue
    - Was a problem pagination.
    - Fixed Issue #10

--- services/visit_canvas_api.rb
+++ services/visit_canvas_api.rb
@@ -2,11 +2,27 @@
 class VisitCanvasAPI
   def initialize(url, canvas_token)
     @url = url
-    @canvas_token = canvas_token
+    @headers = { 'authorization' => ('Bearer ' + canvas_token) }
+  end
+
+  def all_the_data(results, parsed_results, page = 1)
+    links = results.headers.to_hash['link']
+    return parsed_results if links.nil?
+    while links[0].include?('rel="next"')
+      page += 1
+      results = (HTTParty.get "#{@url}?page=#{page}", headers: @headers)
+      parsed_results += JSON.parse(results.to_json)
+      links = results.headers.to_hash['link']
+    end
+    parsed_results
   end
 
   def call
-    headers = { 'authorization' => ('Bearer ' + @canvas_token) }
-    (HTTParty.get @url, headers: headers).to_json
+    results = (HTTParty.get "#{@url}?page=1", headers: @headers)
+    parsed_results = JSON.parse(results.to_json)
+    if parsed_results.is_a?(Array)
+      parsed_results = all_the_data(results, parsed_results)
+    end
+    parsed_results.to_json
   end
 end
